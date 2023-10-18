7fb5b23c65fe44ae5df48946370edbb5cb0083fa
arturo-source <arturojuan9898@gmail.com>
2023-10-18 17:11:47 +0200

    because fl0 deploy fails if you are not listening

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
 	telegrambot "github.com/arturo-source/tramalicantebot/telegram-bot"
 )
 
 func main() {
+	// because fl0 deploy fails if you are not listening
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
 	err := telegrambot.Run()
 	if err != nil {
 		panic(err)
